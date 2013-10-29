4b128835cd1b65012cd0e88ce51ca5b62281a0a7
cschroed <chris@app47.com>
2013-10-29 12:59:13 -0400

    Updated to look more like the results of the standard terminal adapter. Next is to move the methods to the parent class for reuse.

--- src/com/windward/att/commands/MspEnclosureCommand.java
+++ src/com/windward/att/commands/MspEnclosureCommand.java
@@ -20,23 +20,30 @@ import java.util.ArrayList;
  * To change this template use File | Settings | File Templates.
  */
 public class MspEnclosureCommand extends AbstractCommand  {
-    private XML lines=null;
-    @Override
-    public XML executeCommand(XML requestXML) throws Exception {
+
+    protected SSHClient clientFromXML(XML clientXML) throws Exception{
         SSHClient client = sshClientInstance();
         client.addHostKeyVerifier(new HostKeyVerifier() {
             @Override
             public boolean verify(String s, int i, PublicKey publicKey) {
-                return true;  //To change body of implemented methods use File | Settings | File Templates.
+                return true;  // Allow any host, don't verify the key.
             }
         });
+        String num = clientXML.getChild("port").getText();
+        int port = 22;
+        if (num!=null)
+            port = Integer.parseInt(num);
 
-        client.connect("10.174.223.254",22);
-        client.authPassword("cs7866", "1Changeme");
-        System.out.println("Staring session");
+
+        client.connect(clientXML.getChild("host").getText(), port);
+        client.authPassword(clientXML.getChild("username").getText(), clientXML.getChild("password").getText());
+        return client;
+    }
+    @Override
+    public XML executeCommand(XML requestXML) throws Exception {
+        SSHClient client = this.clientFromXML(requestXML.getChild("jump-host"));
         final Session session = client.startSession();
-        System.out.println("Started");
-        lines = new XML("lines");
+        XML commandsOutput = new XML("commands-output");
         try {
             session.allocateDefaultPTY();
             Session.Shell shell = session.startShell();
@@ -44,35 +51,68 @@ public class MspEnclosureCommand extends AbstractCommand  {
             OutputStream ops = shell.getOutputStream();
             PrintStream ps = new PrintStream(ops, true);
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(shell.getInputStream()));
+            ArrayList<String> prompts = new ArrayList<String>(6);
+            XML enclosure = requestXML.getChild("enclosure");
+            String enclosureName = enclosure.getChild("name").getText();
+            prompts.add("Escape character is");      // 0
+            prompts.add(enclosureName + " login:");   // 1
+            prompts.add("Password:");                // 2
+            String enclosurePrompt = enclosureName + ">";
+            prompts.add(enclosurePrompt);         // 3
 
-            ps.println("telnet psloats02 2271");
-            if (readUntilPromptFound(bufferedReader, "Escape character is")){
-                ps.println();
-                if (readUntilPromptFound(bufferedReader, "PSLNZ01MSP2oa01 login:")) {
-                    ps.println("Administrator");
-                    if (readUntilPromptFound(bufferedReader, "Password:")) {
-                        ps.println("changeme");
-                        if (readUntilPromptFound(bufferedReader, "PSLNZ01MSP2oa01>")) {
-                            readUntilPromptFound(bufferedReader, "PSLNZ01MSP2oa01>");
-                            ps.println("show enclosure info");
-                            readUntilPromptFound(bufferedReader, "PSLNZ01MSP2oa01>", true);
+            XML terminalServer = requestXML.getChild("terminal-server");
+            ps.println("telnet " + terminalServer.getChild("host").getText() + " " + terminalServer.getChild("port").getText());
+            int promptFound = readUntilPromptFound(bufferedReader, prompts);;
+            boolean loggedIn = false;
+            do {
+                promptFound = readUntilPromptFound(bufferedReader, prompts);
+                switch (promptFound)    {
+                    case 0:
+                        ps.println();
+                        break;
+                    case 1:
+                        ps.println(enclosure.getChild("username").getText().trim());
+                        break;
+                    case 2:
+                        ps.println(enclosure.getChild("password").getText().trim());
+                        loggedIn = true;
+                        break;
+                    case 3:
+                        if (loggedIn){
+                            XML commands = requestXML.getChild("commands");
+                            for (XML command: commands.getChildren()){
+                                XML commandOutput = new XML("command-output");
+                                readUntilPromptFound(bufferedReader, enclosurePrompt);
+                                ps.println(command.getText());
+                                XML output = new XML("output");
+                                long startTime = System.currentTimeMillis();
+                                boolean found = readUntilPromptFound(bufferedReader, enclosurePrompt, output);
+                                long duration = System.currentTimeMillis() - startTime;
+
+                                XML metadata = new XML("metadata");
+                                metadata.addChild("command").setText(command.getText());
+                                metadata.addChild("line-count").setText(""+output.getChildren().length);
+                                metadata.addChild("execution-milliseconds").setText(""+duration);
+                                metadata.addChild("success").setText(found ? "success" : "failure");
+
+                                commandOutput.addChild(metadata);
+                                commandOutput.addChild(output);
+                                commandsOutput.addChild(commandOutput);
+
+                            }
+
+                            promptFound = -1;           // bail out of the while loop
                         } else {
-                            System.out.println("Couldn't find enclosure prompt");
+                            ps.println("exit");
                         }
-                    } else {
-                        System.out.println("Couldn't find password prompt");
-                    }
-                } else {
-                    System.out.println("Couldn't find login prompt");
-                }
-            } else {
-                System.out.println("Couldn't find tty prompt");
-            }
 
-            System.out.println("Done!!");
+                        break;
+                }
+            } while (promptFound>=0);
+            ps.println("exit"); // Exit out of the enclousre prompt
             ps.println(new byte[]{29}); //what is CTRL-]?
-            ps.println("quit");
-            return lines;
+            ps.println("quit"); // Quit out of the terminal session.
+            return commandsOutput;
         } finally {
             session.close();
             client.disconnect();
@@ -81,19 +121,21 @@ public class MspEnclosureCommand extends AbstractCommand  {
     private boolean readUntilPromptFound(BufferedReader bufferedReader, String prompt){
         ArrayList<String> prompts = new ArrayList<String>(1);
         prompts.add(prompt);
-        return  0==readUntilPromptFound(bufferedReader, prompts, 100000, false);
+        return  0==readUntilPromptFound(bufferedReader, prompts, 10000, null);
     }
-    private boolean readUntilPromptFound(BufferedReader bufferedReader, String prompt, boolean captureOutput){
+    private boolean readUntilPromptFound(BufferedReader bufferedReader, String prompt, XML output){
         ArrayList<String> prompts = new ArrayList<String>(1);
         prompts.add(prompt);
-        return  0==readUntilPromptFound(bufferedReader, prompts, 100000, captureOutput);
+        return  0==readUntilPromptFound(bufferedReader, prompts, 10000, output);
     }
-
-   private int readUntilPromptFound( BufferedReader bufferedReader, ArrayList<String> prompts, long timeout, boolean captureOutput){
+   private int readUntilPromptFound( BufferedReader bufferedReader, ArrayList<String> prompts){
+       return readUntilPromptFound(bufferedReader, prompts, 10000, null);
+   }
+   private int readUntilPromptFound( BufferedReader bufferedReader, ArrayList<String> prompts, long timeout, XML output){
         int found = -1;
         long finishTime = System.currentTimeMillis()+timeout;
        StringBuilder buf = new StringBuilder();
-
+       int lineIndex = 0;
         try {
             String line = null;
             while ( (found==-1) && (System.currentTimeMillis() <= finishTime)){
@@ -102,8 +144,13 @@ public class MspEnclosureCommand extends AbstractCommand  {
                     switch (character){
                         case 10:
                         case 13:
-                            if (captureOutput && line!=null && line.length()>0)
-                                lines.addChild(new XML("line").setText(line));
+                            if (output!=null && line!=null && line.length()>0)   {
+                                XML lineXML = new XML("line");
+                                lineXML.setAttribute("index",lineIndex);
+                                lineXML.setText(line);
+                                output.addChild(lineXML);
+                                lineIndex++;
+                            }
                             line = null;
                             buf.delete(0, buf.length());
                             // clear out buffer array
